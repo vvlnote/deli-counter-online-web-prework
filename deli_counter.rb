27f02e882874553b2dbe9c8316c5494194ac9eda
Joy Lin <vvlnote@hotmail.com>
2019-02-15 06:41:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,5 +5,7 @@ def line
   if line.size == 0
     puts "The line is currently empty."
   else
-    str = ""
+    str = "The line is currently: "
+    for i in 0...line.size do
+      str += "#{i+1}."
 end
