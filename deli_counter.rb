dc66d92e97b7287ebd9337539efdc436d8f20868
Joy Lin <vvlnote@hotmail.com>
2019-02-15 06:45:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,10 @@
 katz_deli = []
 
 def line(array)
+  str = "The line is currently:"
   if array.size == 0
     puts "The line is currently empty."
   else
-    str = "The line is currently:"
     for i in 0...array.size do
       str += " #{i+1}. #{array[i]}"
     end
